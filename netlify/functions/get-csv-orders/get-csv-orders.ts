0b2fcbe8179c0817d5145f5d3a8fa09bc9efb842
Kevin Miguet <kevin.miguet@gmx.fr>
2022-06-22 13:40:55 +0200

    d

--- netlify/functions/get-csv-orders/get-csv-orders.ts
+++ netlify/functions/get-csv-orders/get-csv-orders.ts
@@ -61,6 +61,6 @@ export const handler: Handler = async (event, context) => {
   const response = result;
   return {
     statusCode: 200,
-    body: JSON.stringify(response),
+    body: response,
   };
 };
